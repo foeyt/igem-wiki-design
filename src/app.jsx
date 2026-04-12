6ac6a0e2fb01b4c7e949a9a96d0044f0cc4542c0
foeyt <foeyt522@gmail.com>
2026-04-12 18:47:47 +0800

    [Fix] Fixed branch conflict.

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,31 @@
-import { useState } from "react";
-import "./styles/app.css";
-import Navbar from "./components/navbar/navbar";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import Description from "./pages/Description";
+import Project from "./pages/Project";
+import WetLab from "./pages/WetLab";
+import Team from "./pages/Team";
+
+import BackToTopButton from "./components/BackToTopButton";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <main></main>
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/description" element={<Description />} />
+        <Route path="/project" element={<Project />} />
+        <Route path="/wetlab" element={<WetLab />} />
+        <Route path="/team" element={<Team />} />
+      </Routes>
+      <BackToTopButton/>
+      <Footer />
+    </>
   );
 }
 
